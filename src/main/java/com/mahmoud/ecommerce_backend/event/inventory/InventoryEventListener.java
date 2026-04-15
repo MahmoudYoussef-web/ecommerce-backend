7cf84ff029ba2d8915dd232d7c2dedd65381d392
MahmoudYoussef-web <lime24864@gmail.com>
2026-04-15 12:54:43 +0200

    feat(events): introduce event-driven architecture
    - Add PaymentCompletedEvent and OrderCreatedEvent
    - Implement Payment, Inventory, and Finance listeners
    - Ensure idempotent processing
    - Decouple payment from order and inventory logic

--- src/main/java/com/mahmoud/ecommerce_backend/event/inventory/InventoryEventListener.java
+++ src/main/java/com/mahmoud/ecommerce_backend/event/inventory/InventoryEventListener.java
@@ -1,4 +1,48 @@
 package com.mahmoud.ecommerce_backend.event.inventory;
 
+import com.mahmoud.ecommerce_backend.entity.*;
+import com.mahmoud.ecommerce_backend.enums.StockMovementType;
+import com.mahmoud.ecommerce_backend.repository.StockMovementRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
 public class InventoryEventListener {
+
+    private final StockMovementRepository stockMovementRepository;
+
+    @EventListener
+    @Transactional
+    public void handleOrderCreated(OrderCreatedEvent event) {
+
+        Order order = event.getOrder();
+
+        for (OrderItem item : order.getOrderItems()) {
+
+            int quantity = item.getQuantity();
+
+            StockMovement movement = StockMovement.builder()
+                    .productId(item.getProductId())
+                    .variantId(item.getVariantId())
+                    .movementType(StockMovementType.ORDER_OUT)
+                    .quantity(quantity)
+                    .beforeQuantity(null)
+                    .afterQuantity(null)
+                    .referenceId(order.getId())
+                    .referenceType("ORDER")
+                    .note("Order #" + order.getOrderNumber())
+                    .build();
+
+            stockMovementRepository.save(movement);
+        }
+
+        log.info("Stock movements created | orderId={} itemsCount={}",
+                order.getId(),
+                order.getOrderItems().size());
+    }
 }
--- src/main/java/com/mahmoud/ecommerce_backend/event/inventory/OrderCreatedEvent.java
+++ src/main/java/com/mahmoud/ecommerce_backend/event/inventory/OrderCreatedEvent.java
@@ -1,4 +1,16 @@
 package com.mahmoud.ecommerce_backend.event.inventory;
 
-public class OrderCreatedEvent {
+import com.mahmoud.ecommerce_backend.entity.Order;
+import lombok.Getter;
+import org.springframework.context.ApplicationEvent;
+
+@Getter
+public class OrderCreatedEvent extends ApplicationEvent {
+
+    private final Order order;
+
+    public OrderCreatedEvent(Object source, Order order) {
+        super(source);
+        this.order = order;
+    }
 }
